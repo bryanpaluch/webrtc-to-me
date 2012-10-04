ddb9649ee0c06ba007a64b8dcc1d3c1e950e8835
root <root@commapi.(none)>
2012-10-04 09:00:16 -0700

    fixed example config

--- config/config.example.js
+++ config/config.example.js
@@ -1,12 +1,13 @@
 
 module.exports = {
     development: {
-      db: 'mongodb://localhost/cmmaybe',
+      db: 'mongodb://localhost/webrtc-me',
       twitter: {
           consumerKey: ""
         , consumerSecret: ""
-        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+        , callbackURL: "http://10.255.132.197:3000/auth/twitter/callback"
       },
+			shortUrl: 'http://10.255.132.183:3000/c/'
     }
   , test: {
 
