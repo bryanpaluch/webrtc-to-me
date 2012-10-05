da9ab6a697708f979e905aff9cbe55c48029368e
bryanpaluch <bryanpaluch@gmail.com>
2012-10-05 11:58:29 -0700

    cleaning up settings.js

--- settings.js
+++ settings.js
@@ -42,34 +42,6 @@ function bootApplication(app, config, passport) {
       res.locals.title = 'RTC With Me'
       res.locals.showStack = app.showStackError
       res.locals.req = req
-      res.locals.formatDate = function (date) {
-        var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec" ]
-        return monthNames[date.getMonth()]+' '+date.getDate()+', '+date.getFullYear()
-      }
-      res.locals.stripScript = function (str) {
-        return str.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
-      }
-      res.locals.createPagination = function (pages, page) {
-        var url = require('url')
-          , qs = require('querystring')
-          , params = qs.parse(url.parse(req.url).query)
-          , str = ''
-
-        params.page = 0
-        var clas = page == 0 ? "active" : "no"
-        str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">First</a></li>'
-        for (var p = 1; p < pages; p++) {
-          params.page = p
-          clas = page == p ? "active" : "no"
-          str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">'+ p +'</a></li>'
-        }
-        params.page = --p
-        clas = page == params.page ? "active" : "no"
-        str += '<li class="'+clas+'"><a href="?'+qs.stringify(params)+'">Last</a></li>'
-
-        return str
-      }
-
       next()
     })
 
